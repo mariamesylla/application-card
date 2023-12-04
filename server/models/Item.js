6e8e4eb941543fa757dc3a61e522461466550f49
Magnan Sylla <sylmar956@gmail.com>
2023-12-04 13:43:24 -0500

    Model Item (#2)

---
+++ server/models/Item.js
@@ -0,0 +1,22 @@
+const {sequelize, DataTypes} = require('../db');
+
+const Item = sequelize.define('Item',{
+    
+    
+    name: DataTypes.STRING,
+    
+    description: DataTypes.STRING,
+
+    price: DataTypes.FLOAT,
+
+    category: DataTypes.STRING,
+
+    image: DataTypes.STRING
+    
+
+})
+
+
+module.exports = {
+    Item
+};
