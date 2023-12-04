77f78a44b4a5fa7f1a3dcdd8fba42ff8c106e2f0
Nicholas Botorog <nicholasbotorog@gmail.com>
2023-12-04 17:37:05 -0500

    Added Style.css into Index.js(inside components)

--- public/react/index.js
+++ public/react/index.js
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import 'regenerator-runtime/runtime'
 import {App} from './components/App';
 import { BrowserRouter } from "react-router-dom";
+import '../../public/style.css'
 
 const root = createRoot(document.getElementById("root"));
 root.render(
