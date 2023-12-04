398e37d3c8e86d4ab22f89409ec031db2a671b88
Hector Pulido <87735459+hepulido@users.noreply.github.com>
2023-12-04 13:44:03 -0500

    Feature: Created findAll and findOne routes for items (#1)

---
+++ server/routes/items.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const { Item } = require("../models");
+
+// GET /items
+router.get("/", async (req, res, next) => {
+  try {
+    const items = await Item.findAll();
+    res.send(items);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Get /:id
+router.get('/:id', async (req, res, next) => {
+    try {
+      const item = await Item.findByPk(req.params.id)
+      if (!item) return res.sendStatus(404)
+      res.send(item)
+    } catch (err) {
+      next(err);
+    }
+  })
+
+module.exports = router;
