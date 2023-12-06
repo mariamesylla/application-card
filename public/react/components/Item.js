22fa36411b7a3940934de074f3f5541a7b39e552
Hector Pulido <87735459+hepulido@users.noreply.github.com>
2023-12-06 16:43:12 -0500

    Dev hector 1 (#27)
    * fixed routes
    
    * Feature: install react-router
    
    * Fixed update route
    
    * fixed problem
    
    * Cleaned code comments
    
    * Feature: Created update request on UpdateItem component
    
    * re-pushing update item button
    
    ---------
    
    Co-authored-by: Kaikaine <37594711+Kaikaine@users.noreply.github.com>

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -42,23 +42,26 @@ export const ItemContainer = () => {
   };
 
 
-  return (
-
-    <div className=''>
-        <div className='mx-auto px-4 sm:px-6 sm:py-24 lg:max-w-5xl lg:px-10 sm:max-w-full flex flex-col'>
-            {/* container-item */}
-            <div className='flex items-center mt-5'>
-                {/* container Image */}
-                <div className=''>
-                    <img className='h-full w-full object-cover object-center lg:max-w-full' src={item.image} alt='Item Image' />
+    return (
+        <div className=''>
+            <div className='mx-auto px-4 sm:px-6 sm:py-24 lg:max-w-5xl lg:px-10 sm:max-w-full flex flex-col'>
+                {/* container-item */}
+                <div className='flex items-center mt-5'>
+                    {/* container Image */}
+                    <div className=''>
+                        <img className='h-full w-full object-cover object-center lg:max-w-full' src={item.image} alt='Item Image' />
+                    </div>
+                    {/* description page */}
+                    <div className='flex flex-col ml-20'>
+                        <h2 className='font-bold text-xl mb-1'>{item.name}</h2>
+                        <hr />
+                        <p className='mt-12'>{item.description}</p>
+                        <button
+                            className='mt-2 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-red-600'>
+                            <Link to={`/updateItem/${item.id}`}> Update</Link>
+                        </button>
+                    </div>
                 </div>
-                {/* description page */}
-                <div className='flex flex-col ml-20'>
-                    <h2 className='font-bold text-xl mb-1'>{item.name}</h2>
-                    <hr />
-                    <p className='mt-12'>{item.description}</p>
-                </div>
-            </div>
 
             {/* BUTTONS */}
             <div className='flex justify-end w-[30%] bg-black'>
