b05138c5dfeb235936a8808a143ead8828717d9b
Hector Pulido <87735459+hepulido@users.noreply.github.com>
2023-12-07 15:37:28 -0500

    Dev hector (#37)
    * Feature: Created findAll and findOne routes for items
    
    * Feature: added post and delete to items routes
    
    * Feature: added navigation to create items and update items forms
    
    * Created style for createItem and updateItem
    
    ---------
    
    Co-authored-by: Nicholas <103049873+NicholasBotorog@users.noreply.github.com>

--- public/react/components/CreateItem.js
+++ public/react/components/CreateItem.js
@@ -1,82 +1,169 @@
 
-import React, {useState,useEffect} from 'react';
-import { useParams, Link, useNavigate } from 'react-router-dom'
-import apiURL from '../api';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import apiURL from "../api";
+import { Link } from 'react-router-dom'
 
 <link rel="stylesheet" href="../../style.css" />
 
 export const CreateItem = () => {
-const [name, setName] = useState('')
-const [description, setDescription]= useState('')
-const [price, setPrice] = useState (0)
-const [category, setCategory] = useState('')
-const [image, setImage] = useState ('')
 
-const navigate = useNavigate();
-
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
-		e.preventDefault()
-        try {
-		    const res = await fetch(`${apiURL}/items`,{
-             method: "POST",
-             headers: {
-                "Content-Type": "application/json",
-             },
-             body: JSON.stringify({
-                name,
-                description,
-                price,
-                category,
-                image,
-             })
-             })
-			const responseData = await res.json()
-            if(responseData.ok){
-                setName('')
-                setDescription('')
-                setPrice(0)
-                setCategory('')
-                setImage('')  
-            }
-            navigate('/items')
-		} catch (error) {
-			console.log(error)
-		}
-        navigate("/items");
-	}
+    e.preventDefault();
+    try {
+      const res = await fetch(`${apiURL}/items`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          description,
+          price,
+          category,
+          image,
+        }),
+      });
+      const responseData = await res.json();
+      if (responseData.ok) {
+        setName("");
+        setDescription("");
+        setPrice(0);
+        setCategory("");
+        setImage("");
+      }
+      navigate("/items");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
 
 
   return (
-   <>
-   
-    <h2>Add Items</h2>
-    <div  className="form-container">
-   
+    <>
+    <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-full lg:px-20'>
     <form onSubmit={handleSubmit}>
-    <label className="input-label">Name:
-    <br/>
-        <input className="input field" type="text" placeholder='name' value={name} onChange={(e) => setName(e.target.value)} />
-    </label>
-    <label className="input-label">Description:
-    <br/>
-        <textarea type="" placeholder='description' value={description} onChange={(e) => setDescription(e.target.value)} />
-    </label>
-    <label className="input-label">Price:
-    <br/>
-        <input className="input field" type="number" placeholder='Price' value={price} onChange={(e) => setPrice(e.target.value)} />
-    </label>
-    <label className="input-label">Category:
-    <br/>
-        <input className="input field" type="text" placeholder='category' value={category} onChange={(e) => setCategory(e.target.value)} />
-    </label>
-    <label className="input-label">Image:
-    <br/>
-        <input className="input field" type="text" placeholder='image' value={image} onChange={(e) => setImage(e.target.value)} />
-    </label>
-    <button  className="submit-button" type="submit"> Submit </button>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Add Item</h2>
+          <p className="mt-1 text-sm leading-6 text-gray-600">
+            This Item will be displayed publicly so be careful what you share.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+                Name
+              </label>
+              <div className="mt-2">
+                <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                  <input
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    id="name"
+                    autoComplete="name"
+                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    placeholder="Name of your Item"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-full">
+              <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                Description
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  rows={3}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+
+                />
+              </div>
+            </div>
+        </div>
+        <div className="border-b border-gray-900/10 pb-12">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
+                Price
+              </label>
+              <div className="mt-2">
+                <input
+                  type="number"
+                  name="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  id="price"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
+                Category
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                  id="category"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
+                Image
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="image"
+                  id="image"
+                  value={image}
+                  onChange={(e) => setImage(e.target.value)}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900" >
+         <Link to={'/items'}>Cancel</Link>
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Save
+        </button>
+      </div>
     </form>
-   </div>
-   </>
+    </div>
+    </>
   )
-} 
+}
+ 
