bd56e99a1ec9a16f392db02dbb4bd4e283da5bf9
hector <hpulido1021@gmail.com>
2023-12-08 13:10:52 -0500

    Moved Navbar to app.js

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,23 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 // import and prepend the api url to any fetch calls
-import { Items } from '../components/ItemsContainer'
-import { ItemContainer } from '../components/Item';
-import { SaucesList } from './SaucesList';
-import { Home } from './Home';
-import { CreateItem } from './CreateItem'
-import { UpdateItem } from './UpdateItem';
+import { Items } from "../components/ItemsContainer";
+import { ItemContainer } from "../components/Item";
+import { SaucesList } from "./SaucesList";
+import { Home } from "./Home";
+import { CreateItem } from "./CreateItem";
+import { UpdateItem } from "./UpdateItem";
+import { Navbar } from "./Navbar";
 
 export const App = () => {
-	return (
-		<Routes>
-			<Route  path="/" element={<Home />} />
-			<Route  path="/items" element={<Items />} />
-			<Route  path="/items/:id" element={<ItemContainer />} />
-			<Route  path="/sauces" element={<SaucesList />} />
-			<Route  path="/createItem" element={<CreateItem />} />
-			<Route  path="/updateItem/:id" element={<UpdateItem />} />
-		</Routes>
-	)
-}
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/items" element={<Items />} />
+        <Route path="/items/:id" element={<ItemContainer />} />
+        <Route path="/sauces" element={<SaucesList />} />
+        <Route path="/createItem" element={<CreateItem />} />
+        <Route path="/updateItem/:id" element={<UpdateItem />} />
+      </Routes>
+    </>
+  );
+};
--- public/react/index.js
+++ public/react/index.js
@@ -4,13 +4,10 @@ import 'regenerator-runtime/runtime'
 import {App} from './components/App';
 import { HashRouter } from "react-router-dom";
 import '../../public/style.css'
-import { Navbar } from "./components/Navbar";
-// import '../../public/style.css'
 
 const root = createRoot(document.getElementById("root"));
 root.render(
     <HashRouter>
-        <Navbar />
         <App />
     </HashRouter>
 );
